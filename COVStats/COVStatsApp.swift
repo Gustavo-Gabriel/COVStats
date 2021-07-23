7465f5694bf0702036a108838702dbe790ac7cd9
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-07-23 19:50:25 -0400

    Initial Commit

---
+++ COVStats/COVStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVStatsApp.swift
+//  COVStats
+//
+//  Created by Gustavo Anjos on 23/07/21.
+//
+
+import SwiftUI
+
+@main
+struct COVStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COVStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVStats
+//
+//  Created by Gustavo Anjos on 23/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
